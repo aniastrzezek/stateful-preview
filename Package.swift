41d1827454c878ac97f9cca91c555add3db1366e
Anna Strzeżek <astrzezek@gmail.com>
2024-04-13 17:24:39 +0200

    Clean package file

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "StatefulPreview",
+    name: "stateful-preview",
     platforms: [.iOS(.v15)], // TODO: check the rest of the platforms
     products: [
         .library(
